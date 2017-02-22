829e8d1e936f05a730bb96b180d941a678081249
weiliango <liweilia@usc.edu>
2017-02-22 11:39:21 -0800

    refactor the project structure

---
+++ Assets/Scripts/AI/Monitors/TowerMonitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Application = UnityEngine.Application;
+
+// tower location, rank, attack, defend... properities
+
+namespace AI.Monitors
+{
+	public class TowerMonitor : Monitor
+	{
+			
+	}
+}
+
