1769674049993001fcfc9e9e7370cb7b62fc3890
fanf <fanf@oupeng.com>
2017-04-03 15:56:26 -0700

    new folder

---
+++ Assets/Scripts/AI/EA/GeneNode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class GeneNode {
+	public int[][] pos;
+	public int towerIndex;
+
+	public GeneNode(int[][] pos, int towerIndex) {
+		this.pos = pos;
+		this.towerIndex = towerIndex;
+	}
+}
---
+++ Assets/Scripts/AI/EA/GeneSeq.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GeneSeq {
+	private List<GeneNode> seqList = new List<GeneNode>();
+	public int id;
+
+
+	public void AddNodes(List<GeneNode> nodes) {
+		int size = nodes.Count;
+		for (int i = size - 1; i >= 0; i--) {
+			seqList.Add (nodes[i]);
+		}
+	}
+
+	public GeneNode GetNodeByIndex(int index) {
+		if (index >= this.Size ()) {
+			return null;
+		}
+		return seqList [this.Size () - index - 1];
+	}
+
+	public void SetNode(int index, int[][] pos, int towerIndex) {
+		if (index >= this.Size ()) {
+			return null;
+		}
+		seqList [this.Size () - index - 1].pos = pos; 
+		seqList [this.Size () - index - 1].towerIndex = towerIndex; 
+	}
+
+	public void SetNode(int index, int[][] pos) {
+		if (index >= this.Size ()) {
+			return null;
+		}
+		seqList [this.Size () - index - 1].pos = pos; 
+	}
+
+	public void SetNode(int index, int towerIndex) {
+		if (index >= this.Size ()) {
+			return null;
+		}
+		seqList [this.Size () - index - 1].towerIndex = towerIndex; 
+	}
+
+	public GeneNode getNextStep() {
+		int size = this.Size ();
+		if (size <= 0) {
+			return null;
+		} 
+		GeneNode node = seqList [size - 1];
+		seqList.RemoveAt(size - 1);
+		return node;
+	}
+
+	public int Size() {
+		return seqList.Count;
+	}
+}
