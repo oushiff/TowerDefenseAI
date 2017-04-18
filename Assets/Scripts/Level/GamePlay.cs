4ded101287161146c9bf7c10518b5460cdc0100a
oushiff <fanfei@usc.edu>
2017-04-17 21:34:28 -0700

    logtool update

--- Assets/Scripts/Level/GamePlay.cs
+++ Assets/Scripts/Level/GamePlay.cs
@@ -261,6 +261,8 @@ public class GamePlay : MonoBehaviour
     public void EndLevel()
     {
         
+		currency.RecordEndMoneyLog ();
+
 
         UIManager.instance.ShowEndOfLevel();
 
--- logtool/logtool.sh
+++ logtool/logtool.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-rm logdata
+LOG_DATA=/Users/Franz/Library/Logs/Unity/Editor.log
+TMP=/Users/Franz/Documents/524LoopControl/LoopControl/tmp_mid
+RES=/Users/Franz/Documents/524LoopControl/LoopControl/tmp_log
+echo `strings $LOG_DATA` > $TMP
+echo "" > $RES
 for i in `cat logtool.conf`;
 do
-    if grep -q $i ./../Log_*;
+    if grep -q $i $TMP;
     then
-        echo `grep $i ./../Log_*` >> logdata
+        echo `grep -q $i $TMP` >> $RES
     fi
 done
+
